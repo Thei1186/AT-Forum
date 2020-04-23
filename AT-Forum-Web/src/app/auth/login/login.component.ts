812c677405f8a69d9bcb109f6409355380ff06f9
Asvør <asvor_rasmussen@hotmail.com>
2020-04-23 12:47:49 +0200

    login

--- AT-Forum-Web/src/app/auth/login/login.component.ts
+++ AT-Forum-Web/src/app/auth/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Store} from '@ngxs/store';
+import {Observable} from 'rxjs';
+import {User} from '../../users/shared/user';
+import {SignUp} from '../shared/auth.action';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  newLoginForm: FormGroup;
+  user: User;
+  username: string;
+  password: string;
 
-  constructor() { }
+  constructor(private store: Store, private fb: FormBuilder) { }
 
   ngOnInit() {
+
   }
 
+  login() {
+  
+  }
 }

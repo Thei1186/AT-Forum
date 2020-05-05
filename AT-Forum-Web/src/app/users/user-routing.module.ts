73a236019f18d6fde5624e6c0dc6a89d551c8f24
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2020-05-05 15:24:21 +0200

    Guard.. doesn't work

--- AT-Forum-Web/src/app/users/user-routing.module.ts
+++ AT-Forum-Web/src/app/users/user-routing.module.ts
@@ -5,12 +5,13 @@ import { ProfileComponent } from './profile/profile.component';
 import {CreateUserComponent} from './create-user/create-user.component';
 import {AdminPageComponent} from './admin-page/admin-page.component';
 import {EditUserComponent} from './edit-user/edit-user.component';
+import {AuthGuard} from '../auth/guard/auth.guard';
 
 const routes: Routes = [
-  { path: 'profile', component: ProfileComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
   { path: 'create-user', component: CreateUserComponent},
-  { path: 'edit-user/:id', component: EditUserComponent},
-  { path: 'admin', component: AdminPageComponent}
+  { path: 'edit-user/:id', component: EditUserComponent, canActivate: [AuthGuard]},
+  { path: 'admin', component: AdminPageComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({

4fcad61f6686072c8698d562c4e2a93536407d8e
Asvør <asvor_rasmussen@hotmail.com>
2020-05-06 12:46:21 +0200

    more refactoring

--- AT-Forum-Web/src/app/posts/posts.module.ts
+++ AT-Forum-Web/src/app/posts/posts.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PostsRoutingModule } from './posts-routing.module';
-import { PostsComponent } from './posts.component';
 
 
 @NgModule({
-  declarations: [PostsComponent],
+  declarations: [],
   imports: [
     CommonModule,
     PostsRoutingModule

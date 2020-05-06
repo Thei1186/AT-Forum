be44639e9cb2ce8b4752a401a464957199aaed9c
Asvør <asvor_rasmussen@hotmail.com>
2020-05-06 12:47:24 +0200

    Update posts-routing.module.ts

--- AT-Forum-Web/src/app/posts/posts-routing.module.ts
+++ AT-Forum-Web/src/app/posts/posts-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { PostsComponent } from './posts.component';
-
-const routes: Routes = [{ path: '', component: PostsComponent }];
+const routes: Routes = [{}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

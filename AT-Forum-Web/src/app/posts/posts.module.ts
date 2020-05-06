7fe15f37a2c5228c9fb2eedd96991842ecc53f09
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2020-05-06 14:31:09 +0200

    create topic and read all

--- AT-Forum-Web/src/app/posts/posts.module.ts
+++ AT-Forum-Web/src/app/posts/posts.module.ts
@@ -5,10 +5,11 @@ import { PostsRoutingModule } from './posts-routing.module';
 import { CategoryComponent } from './category/category.component';
 import { TopicComponent } from './topic/topic.component';
 import { CommentComponent } from './comment/comment.component';
+import { CreateTopicComponent } from './topic/create-topic/create-topic.component';
 
 
 @NgModule({
-  declarations: [CategoryComponent, TopicComponent, CommentComponent],
+  declarations: [CategoryComponent, TopicComponent, CommentComponent, CreateTopicComponent],
   imports: [
     CommonModule,
     PostsRoutingModule

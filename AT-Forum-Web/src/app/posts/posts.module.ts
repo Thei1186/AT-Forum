6d6e2bb8fea1b4205df0c7a3273a66a24ab59025
Asvør <asvor_rasmussen@hotmail.com>
2020-05-06 13:09:43 +0200

    components created

--- AT-Forum-Web/src/app/posts/posts.module.ts
+++ AT-Forum-Web/src/app/posts/posts.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PostsRoutingModule } from './posts-routing.module';
+import { CategoryComponent } from './category/category.component';
+import { TopicComponent } from './topic/topic.component';
+import { CommentComponent } from './comment/comment.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CategoryComponent, TopicComponent, CommentComponent],
   imports: [
     CommonModule,
     PostsRoutingModule

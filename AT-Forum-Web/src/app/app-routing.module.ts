663b9068d57443c6cb1c2137cb04b4129a8a8015
Asvør <asvor_rasmussen@hotmail.com>
2020-05-06 12:45:43 +0200

    refactoring

--- AT-Forum-Web/src/app/app-routing.module.ts
+++ AT-Forum-Web/src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent},
   { path: 'user', loadChildren: () => import('./users/user.module').then(m => m.UserModule) },
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) }
+  { path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
